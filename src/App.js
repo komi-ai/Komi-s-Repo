8029271f76db8da2137ea67e3a6b88e3c5a0f730
“komi-ai” <ka00049@Mix.wvu.edu>
2024-02-21 13:18:10 -0500

    Add exclamation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800405172</p>
-      <p>Hi, I am Kumail Abbas</p>
+      <p>Hi, I am Kumail Abbas!</p>
     </div>
   );
 }
